e123d14bb71da2f83377fa10d7eec1b415816dc7
SQLAdmin <abuyeahia24@gmail.com>
2023-05-25 01:34:47 +0600

    added review items from mongodb

--- index.js
+++ index.js
@@ -30,11 +30,16 @@ async function run() {
 
 
         const menuCollection = client.db("bistroDb").collection("menu");
+        const reviewCollection = client.db("bistroDb").collection("reviews");
 
         app.get('/menu', async(req,res)=>{
             const result = await menuCollection.find().toArray();
             res.send(result);
-        })
+        });
+        app.get('/reviews', async(req,res)=>{
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
 
 
 
