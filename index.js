9e5feca5b7f7b1ad3968799887ce5d206d53c1fc
SQLAdmin <abuyeahia24@gmail.com>
2023-06-02 12:32:40 +0600

    added api for payment intent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -176,7 +177,7 @@ async function run() {
             }
             const decodedEmail = req.decoded.email;
             if (email !== decodedEmail) {
-                return res.status(403).send({ error: true, message: 'Unauthorized Access' })
+                return res.status(403).send({ error: true, message: 'forbidden Access' })
             }
             else {
                 const query = { email: email };
@@ -200,6 +201,21 @@ async function run() {
             res.send(result);
         })
 
+
+
+        // create payment intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
 
 
 
